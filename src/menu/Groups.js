0745a4b0e1fd319cb07a91f6d4b01cc03a288398
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2018-10-06 14:40:09 +0200

    group formation

--- src/menu/Groups.js
+++ src/menu/Groups.js
@@ -14,7 +14,39 @@ class Groups extends Component {
       <div class="content-desc">
         <h2>Groups</h2>
         <h3>Student/GitHub Repo</h3>
-        <h3>Group formation is not available yet</h3>
+        <h3>Group #1</h3>
+
+        <div class="content-desc">
+            <a href="https://github.com/paredesrichard/">
+                <img src={github} className="github-icon" alt="github-icon"/>
+            </a>
+            <a href="https://www.linkedin.com/in/richard-paredes-4a3a70133/">
+              <img src={linkedin} className="linkedin-icon" alt="linkedin-icon"/>
+            </a>Richard Paredes 
+        </div>
+        <div>
+           <img src="http://ghchart.rshah.org/paredesrichard" alt="paredesrichard's Github chart" />
+       </div>
+       <div class="content-desc">
+            <a href="https://github.com/jacklinenassozi/">
+                <img src={github} className="github-icon" alt="github-icon"/>
+            </a>Jackline Nassozi
+        </div>
+        <div>
+            <img src="http://ghchart.rshah.org/jacklinenassozi" alt="jacklinenassozi's Github chart" />
+       </div>
+       
+       <div class="content-desc">
+            <a href="https://github.com/JVDZMN">
+                <img src={github} className="github-icon" alt="github-icon"/>
+            </a>Javad Zamani        
+        </div>
+        <div>
+           <img src="http://ghchart.rshah.org/JVDZMN" alt="JVDZMN's Github chart" />
+        </div>
+
+        <h3>Group #2</h3>
+        
         <div class="content-desc"> 
             <a href="https://github.com/chandu87/">
                 <img src={github} className="github-icon" alt="github-icon"/>
@@ -24,11 +56,10 @@ class Groups extends Component {
             </a>Chandrashekar Palle 
         </div>
         <div>
-        <img src="http://ghchart.rshah.org/chandu87" alt="chandu87's Github chart" />
+            <img src="http://ghchart.rshah.org/chandu87" alt="chandu87's Github chart" />
         </div>
-        <div>
-           <img src="http://ghchart.rshah.org/Majd-Darraj" alt="Majd-Darraj's Github chart" />
-       </div>
+
+
        <div class="content-desc">
              <a href="https://github.com/majidkhan">
                  <img src={github} className="github-icon" alt="github-icon"/>
@@ -54,7 +85,8 @@ class Groups extends Component {
        </div>
 
 
-
+        <h3>Group #3</h3>
+        
         <div class="content-desc">
             <a href="https://github.com/Milamanieva/">
                   <img src={github} className="github-icon" alt="github-icon"/>
@@ -66,6 +98,7 @@ class Groups extends Component {
         <div>
            <img src="http://ghchart.rshah.org/Milamanieva" alt="Milamanieva's Github chart" />
        </div>
+
        <div class="content-desc">
          <a href="https://github.com/Majd-Darraj">
                 <img src={github} className="github-icon" alt="github-icon"/>
@@ -73,6 +106,9 @@ class Groups extends Component {
              <a href="https://www.linkedin.com/in/majd-darraj-39ab9399/">
                   <img src={linkedin} className="linkedin-icon" alt="linkedin-icon"/>  
               </a>Majd Darraj 
+       </div>
+       <div>
+           <img src="http://ghchart.rshah.org/Majd-Darraj" alt="Majd-Darraj's Github chart" />
        </div>
 
         <div class="content-desc">
@@ -87,38 +123,7 @@ class Groups extends Component {
 
 
 
-       <div class="content-desc">
-            <a href="https://github.com/paredesrichard/">
-                <img src={github} className="github-icon" alt="github-icon"/>
-            </a>
-            <a href="https://www.linkedin.com/in/richard-paredes-4a3a70133/">
-              <img src={linkedin} className="linkedin-icon" alt="linkedin-icon"/>
-            </a>Richard Paredes 
-        </div>
-        <div>
-           <img src="http://ghchart.rshah.org/paredesrichard" alt="paredesrichard's Github chart" />
-       </div>
 
-      <div>
-        <img src="http://ghchart.rshah.org/ibrsal" alt="ibrsal's Github chart" />
-       </div>
-       <div class="content-desc">
-            <a href="https://github.com/jacklinenassozi/">
-                <img src={github} className="github-icon" alt="github-icon"/>
-            </a>Jackline Nassozi
-        </div>
-        <div>
-        <img src="http://ghchart.rshah.org/jacklinenassozi" alt="jacklinenassozi's Github chart" />
-       </div>
-       
-       <div class="content-desc">
-            <a href="https://github.com/JVDZMN">
-                <img src={github} className="github-icon" alt="github-icon"/>
-            </a>Javad Zamani        
-        </div>
-        <div>
-           <img src="http://ghchart.rshah.org/JVDZMN" alt="JVDZMN's Github chart" />
-        </div>
  
 
 
